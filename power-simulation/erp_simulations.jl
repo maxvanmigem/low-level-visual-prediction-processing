39def682c3ff3ed1112b2f2ba20420b98814ff3a
Max Van Migem <m.van.migem@gmail.com>
2025-08-25 10:36:15 +0200

    add initial exploration files

--- power-simulation/erp_simulations.jl
+++ power-simulation/erp_simulations.jl
@@ -22,72 +22,48 @@ events_df = generate_events(StableRNG(1), design);
 first(events_df, 10)
 
 
-# c1 = LinearModelComponent(; basis =);
+free = hanning(0.07,0.080,100)
 
-# basisi = n170()
 
-p1 = LinearModelComponent(;
-    basis = p100(),
-    formula = @formula(0 ~ 1 + attention), 
-    β = [5,1]
+c1 = LinearModelComponent(;
+    basis = free,
+    formula = @formula(0 ~ 1 + attention + position + attention&position), 
+    β = [3,0,-6,-0.72]
 );
 
+# p1 = LinearModelComponent(;
+#     basis = hanning(0.1,0.120,100),
+#     formula = @formula(0 ~ 1 + attention),
+#     β = [1,0.4],
+# );
 n1 = LinearModelComponent(;
     basis = n170(),
-    formula = @formula(0 ~ 1 ),
-    β = [5],
+    formula = @formula(0 ~ 1 + position),
+    β = [0.5,1],
 );
 
-p3 = LinearModelComponent(;
-    basis = p300(),
-    formula = @formula(0 ~ 1 + prediction),
-    β = [5, 0.2],
+p2 = LinearModelComponent(;
+    basis = hanning(0.15,0.3,100),
+    formula = @formula(0 ~ 1 + attention + prediction + position + attention&prediction),
+    β = [5,0,0,-4,0.3],
 );
 
-onset = UniformOnset(; width = 0, offset = 200);
+onset = UniformOnset(; width = 0, offset = 300);
 
-noise = PinkNoise(; noiselevel = 0);
+noise = PinkNoise(; noiselevel = 2);
 
 # Combine the components in a vector
-components = [p1, n1, p3]
+components = [c1, n1, p2]
 
 # Simulate data
 eeg_data, events_df = UnfoldSim.simulate(StableRNG(1), design, components, onset, noise);
 
-f = Figure(size = (1000, 400))
-ax = Axis(
-    f[1, 1],
-    title = "Simulated EEG data",
-    titlesize = 18,
-    xlabel = "Time [samples]",
-    ylabel = "Amplitude [µV]",
-    xlabelsize = 16,
-    ylabelsize = 16,
-    xgridvisible = false,
-    ygridvisible = false,
-)
-
-n_samples = 1400
-lines!(eeg_data[1:n_samples]; color = "black")
-v_lines = [
-    vlines!(
-        [r["latency"]];
-        color = ["orange", "teal"][1+(r["attention"]=="attended")],
-        label = r["attention"],
-    ) for r in
-    filter(:latency => x -> x < n_samples, events_df)[:, ["latency", "attention"]] |>
-    eachrow
-]
-xlims!(ax, 0, n_samples)
-axislegend("Event onset"; unique = true);
-
-current_figure()
-
+# Validate
 m = fit(
     UnfoldModel,
     [
         Any => (
-            @formula(0 ~ 1 + position + attention ),
+            @formula(0 ~ 1 + attention*position*prediction),
             firbasis(τ = [-0.1, 1], sfreq = 100, name = "basis"),
         ),
     ],
@@ -127,8 +103,11 @@ hidespines!(ax_A, :t, :r)
 # Plot B: Marginal effects
 plot_B = plot_erp!(
     gb,
-    effects(Dict(:position => ["up","down"], :attention => ["attended", "unattended"]), m);
-    mapping = (; color = :attention, linestyle = :position, group = :position),
+    effects(Dict(
+            :attention => ["attended", "unattended"],
+            :prediction => ["regular","odd"],
+            :position => ["up","down"]), m);
+    mapping = (; color = :prediction, linestyle = :attention, group = :position),
     legend = (; valign = :top, halign = :right),
     axis = (
         title = "Marginal effects",
@@ -141,6 +120,5 @@ plot_B = plot_erp!(
         ygridvisible = false,
     ),
 )
-
-
+xlims!(current_axis(), 0, 0.3)
 current_figure()
