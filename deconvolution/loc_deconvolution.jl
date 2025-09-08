ad61ce947390d93efbb7717303a72cb6b4b967c7
Max Van Migem <m.van.migem@gmail.com>
2025-09-08 13:51:48 +0200

    deconvolution files

---
+++ deconvolution/loc_deconvolution.jl
@@ -0,0 +1,61 @@
+using Unfold
+using UnfoldSim
+using UnfoldMakie,CairoMakie
+using CSV
+using DataFrames
+using Effects
+using StatsModels
+
+el_list = ["Oz","POz","O1","O2",
+            "PO3","PO4",
+            "P1","Pz","P2",
+            "CP1","CPz","CP2",
+            ]
+
+for i in el_list
+    data_path = "C:/Users/mvmigem/Documents/data/project_2/preprocessed/localiser/mastoid-raw-csv/"
+    raw_path = data_path * "raw-$i/"
+    destination_path = "C:/Users/mvmigem/Documents/data/project_2/overlap_corrected/localiser/$i/"
+    # destination_path = "C:/Users/mvmigem/Documents/data/project_1/overlap_corrected/seq-1-p3/$i/" # for seq 1 p3 analysis
+    if !isdir(destination_path)
+        mkdir(destination_path)
+        # Data paths
+        event_path = data_path * "events/"
+
+        event_dir_list = readdir(event_path)
+        raw_dir_list = readdir(raw_path)
+
+        for (evp,rawp) in zip(event_dir_list,raw_dir_list)
+            # Read in the data
+            data = DataFrame(CSV.File(raw_path*rawp))
+            evts = DataFrame(CSV.File(event_path*evp))
+            # Change the column names to fit toolbox
+            rename!(evts,:sample => :latency)
+            evts.position = string.(evts.position)
+            
+            select!(data,i)
+            # Transform to data to Matrix
+            data_r = vec(Matrix(data))
+            # Define basisfunction
+            basisfunction = firbasis(τ=(-0.1,1),sfreq=512,name="myFIRbasis")
+            # Define the linear formula 
+            f = @formula 0 ~ 1 + position
+            # f = @formula 0 ~ 1 + condition + continuous # note the formulas left side is `0 ~ ` for technical reasons`
+            bfDict = Dict(Any=>(f,basisfunction))
+            # Specify contrasts
+            contrasts = Dict(:position=>EffectsCoding())
+            # Fit
+            m = fit(UnfoldModel,bfDict,evts,data_r,contrasts=contrasts)
+            
+            design = Dict(:position => ["1","2","3","4"])
+            eff = effects(design,m)
+            eff.subject .= string(evts[1,:subject])
+            eff.selected_electrode .= i
+            # Save evoked
+            subject_num = lpad(evts[1,:subject],2,"0")
+            filename = "corrected-$(i)-loc_evoked-$subject_num.csv"
+            CSV.write(destination_path*filename, eff)
+
+        end
+    end
+end
---
+++ deconvolution/test.jl
@@ -0,0 +1,49 @@
+using CSV
+using DataFrames
+using Effects
+using Unfold
+using UnfoldSim
+using UnfoldMakie,CairoMakie
+using StatsModels
+
+data_path = "C:/Users/mvmigem/Documents/data/project_2/preprocessed/mastoid-raw-csv/"
+event_path = data_path * "events/"
+event_dir_list = readdir(event_path)
+evts = DataFrame(CSV.File(event_path*"events-sub-02.csv"))
+raw_path = data_path * "raw-POz/"
+raw_dir_list = readdir(raw_path)
+
+data = DataFrame(CSV.File(raw_path*raw_dir_list[2]))
+
+rename!(evts,:sample => :latency)
+filter!(row -> !(row.event_codes == 99),evts)
+evts.position = string.(evts.position)
+evts.sequence = string.(evts.sequence)
+select!(data,"POz")
+data_r = vec(Matrix(data))
+
+basisfunction = firbasis(τ=(-0.1,1),sfreq=512,name="myFIRbasis")
+f = @formula 0 ~ 1 + sequence + position*feature*attended_feature*unattended_feature
+        # f = @formula 0 ~ 1 + condition + continuous # note the formulas left side is `0 ~ ` for technical reasons`
+bfDict = [Any=>(f,basisfunction)]
+        # Specify contrasts
+contrasts = Dict(:position=>EffectsCoding(),
+                        :sequence=>EffectsCoding(),
+                        :feature=>EffectsCoding(),
+                        :attended_feature=>EffectsCoding(),
+                        :unattended_feature=>EffectsCoding())
+
+m = fit(UnfoldModel,bfDict,evts,data_r,contrasts=contrasts)
+
+setup = string(evts[1,:setup])
+setdown = string(evts[1,:setdown])
+        
+design = Dict(:sequence =>["1","2","3","4"],
+                        :position => [setup,setdown],
+                        :feature => ["angle","rotation"],
+                        :attended_feature => ["regular","odd"],
+                        :unattended_feature => ["regular","odd"])
+
+eff = effects(design,m)
+eff.subject .= string(evts[1,:subject])
+eff.selected_electrode .= "POz"
